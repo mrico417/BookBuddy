810530701ea814ae2867a549fe93af08b1f9fb85
Mario Rico <mario.rico417@gmail.com>
2024-04-22 22:00:48 -0500

    added the routes for each react component

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,1 +1,9 @@
 /* TODO - add your code to create a functional React component that renders a registration form */
+
+export const Register = () => {
+	return (
+		<div className="cls-register-container">
+			<h1>Register</h1>
+		</div>
+	);
+};
