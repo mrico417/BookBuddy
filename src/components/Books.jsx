5dcf063147eaf957fbeffead6105b3d2deca67dd
Mario Rico <mario.rico417@gmail.com>
2024-04-23 19:40:52 -0500

    added new BookCard.jsx component

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,6 +1,7 @@
 /* TODO - add your code to create a functional React component that displays all of the available books in the library's catalog. Fetch the book data from the provided API. Users should be able to click on an individual book to navigate to the SingleBook component and view its details. */
 
 import { useEffect, useState } from "react";
+import { BookCard } from "./BookCard";
 
 const APIURL = `https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books`;
 
@@ -36,10 +37,9 @@ export const Books = ({ token }) => {
 	return (
 		<div className="cls-books-container">
 			<div className="cls-books-list">
-				{/* map over the books array to display */}
-				{books.map((book) => (
-					<h1 key={book.id}>{book.title}</h1>
-				))}
+				{books.map((book) => {
+					return <BookCard key={book.id} book={book} />;
+				})}
 			</div>
 		</div>
 	);
