5c172496def022c34e78a60900fed0ab1f73dda4
Mario Rico <mario.rico417@gmail.com>
2024-04-26 10:23:25 -0500

    bugfix: Reservations was making fetch request although there is no token

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,7 +1,6 @@
 /* TODO - add your code to create a functional React component that renders account details for a logged in user. Fetch the account data from the provided API. You may consider conditionally rendering a message for other users that prompts them to log in or create an account.  */
 
 import { useNavigate } from "react-router-dom";
-import { Reservations } from "./Reservations";
 
 export const Account = ({ token, setToken }) => {
 	const navigate = useNavigate();
@@ -19,7 +18,7 @@ export const Account = ({ token, setToken }) => {
 					<button onClick={() => handleLogoff()}>Log Off</button>
 				</>
 			) : (
-				<></>
+				<h1>Need to login to view Account</h1>
 			)}
 		</div>
 	);
